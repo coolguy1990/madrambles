1ccc11455c33d9ca704dd1becb9f6042611797c3
Kunal Mangaraj <mangaraj.kunal@gmail.com>
2013-12-16 12:36:30 +0530

    Added key

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'hy7hGRJ3xc0ssM3KxPHujXAhcylUpX6C',
 
 	/*
 	|--------------------------------------------------------------------------
