7339e971172b6c76522ac916d330698157389271
Kunal Mangaraj <mangaraj.kunal@gmail.com>
2013-12-18 22:33:03 +0530

    Added PostRepositoryInterface

--- app/MadRambles/Facades/MadRambles.php
+++ app/MadRambles/Facades/MadRambles.php
@@ -7,5 +7,91 @@ use MadRambles\Repositories\PostRepositoryInterface;
 
 class MadRambles
 {
+	/**
+	 * The post repository implementation.
+	 *
+	 * @var MadRambles\PostRepositoryInterface
+	 */
+	protected $postsRepo;
 
+	/**
+	 * Create a new madramble facade instance.
+	 *
+	 * @param \MadRambles\Repositories\PostRepositoryInterface
+	 *
+	 * @return \MadRambles\Facades\MadRambles
+	 */
+	public function __construct(PostRepositoryInterface $postsRepo)
+	{
+		$this->postsRepo = $postsRepo;
+	}
+
+	/**
+	 * Fetch posts
+	 *
+	 * @param array $params
+	 *
+	 * @return Posts
+	 */
+	public function posts($params = array())
+	{
+		return $this->postsRepo->facadeSearch($params);
+	}
+
+	/**
+	 * Fetch all tags
+	 */
+	public function tags()
+	{
+		return $this->postsRepo->allTags();
+	}
+
+	/**
+	 * Generate a route to a named group
+	 *
+	 * @param string $route
+	 * @param mixed  $param
+	 *
+	 * @return string
+	 */
+	public function route($route, $param = null)
+	{
+		if($route === '/')
+		{
+			return route('madramble.index');
+		}
+		else
+		{
+			return \URL::route('madrambles.'.$route, $param);
+		}
+	}
+
+	// public function setupViews()
+	// {
+	// 	View::addLocation(public_path().'/'.);
+	// }
+
+	/**
+	 * Auth the current user.
+	 *
+	 * @return \Illuminate\Auth\Guard
+	 */
+	public function getMadRamblesAuth()
+	{
+		$provider = $this->createEloquentProvider();
+		$guard = new Guard($provider, App::make('session.store'));
+		$guard->setCookieJar(App::make('cookie'));
+
+		return $guard;
+	}
+
+	/**
+	 * @return \Illuminate\Auth\EloquentUserProvider
+	 */
+	protected function createEloquentProvider()
+	{
+		$model = 'MadRambles\Models\User';
+
+		return new EloquentUserProvider(App::make('hash'), $model);
+	}
 }
---
+++ app/MadRambles/Repositories/PostRepositoryInterface.php
@@ -0,0 +1,128 @@
+<?php namespace MadRambles\Repositories;
+
+use DateTime;
+
+interface PostRepositoryInterface
+{
+	/**
+	 * Get all of the posts.
+	 *
+	 * @return array
+	 */
+	public function all();
+
+	/**
+	 * Get all the active posts.
+	 *
+	 * @param integer $per_page
+	 *
+	 * @return array
+	 */
+	public function active($per_page);
+
+	/**
+	 * Get all posts with a tag
+	 *
+	 * @param string  $tag
+	 * @param integer $per_page
+	 *
+	 * @return array
+	 */
+	public function activeByTag($tag, $per_page);
+
+	/**
+	 * Get all the posts with a tag
+	 *
+	 * @param string   $tag
+	 * @param integer  $per_page
+	 *
+	 * @return array
+	 */
+	public function search($search, $per_page);
+
+	/**
+	 * Get a post by its primary key.
+	 *
+	 * @param integer $id
+	 *
+	 * @return Post
+	 */
+	public function find($id);
+
+	/**
+	 * Get a Post by its slug
+	 *
+	 * @param string  $slug
+	 *
+	 * @return Post
+	 */
+	public function findBySlug($slug);
+
+	/**
+	 * Create a new slug
+	 *
+	 * @param string 	$title
+     * @param string    $content
+     * @param string    $slug
+     * @param array     $tags
+     * @param bool      $active
+     * @param integer   $user_id
+     * @param DateTime  $publish_date
+     *
+     * @return Post
+	 */
+    public function create($title, $content, $slug, array $tags, $active, $user_id, DateTime $publish_date);
+
+    /**
+     * Update a post
+     *
+     * @param integer   $int
+     * @param string    $title
+     * @param string    $content
+     * @param string    $slug
+     * @param array     $tags
+     * @param bool      $active
+     * @param integer   $user_id
+     * @param DateTime  $publish_date
+     *
+     * @return Post
+     */
+    public function update($id, $title, $content, $slug, array $tags, $active, $user_id, DateTime $publish_date);
+
+    /**
+     * Delete the post with given ID.
+     *
+     * @param integer $id
+     *
+     * @return void
+     */
+    public function delete($id);
+
+    /**
+     * Get a list of all the tags used by the blog
+     *
+     * @return array
+     */
+    public function allTags();
+
+    /**
+     * Determine if the given post is valid for creating
+     *
+     * @param string $title
+     * @param string $slug
+     *
+     * @return \Illuminate\Support\MessageBad
+     */
+    public function validForCreation($title, $slug);
+
+    /**
+     * Determine if given post is valid for updating
+     *
+     * @param string    $title
+     * @param string    $slug
+     * @param integer   $id
+     *
+     * @return \Illuminate\Support\MessageBag
+     */
+    public function validForUpdate($id, $title, $slug);
+}
